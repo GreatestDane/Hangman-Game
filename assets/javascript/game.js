c30159dda1d48caad5b3447866435cd0e98078ac
Greatest Dane <DaneGoodman85@gmail.com>
2018-04-29 23:58:27 -0700

    Webpage is better now, still gotta code the logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,26 @@
+// alright, let's start by declaring the variables for the game //
+var gameOver = false;
+var wins = 0;
+var losses = 0;
+var guessesLeft = 12;
+var userGuess;
+var randomWord;
 
-
+// Create the array of array's for the words you're going to have //
 var words = [
-    ['m', 'a', 'd', 'd', 'o', 'n', 'n', 'a'],
+    ['n', 'e', 'c', 'r', 'o', 's', 'i', 's'],
     ['r', 'a', 'g', 'e'],
-    ['d', 'e', 'a', 't', 'h']
+    ['d', 'e', 'a', 't', 'h'],
+    ['l', 'u', 'c', 'i', 'f', 'e', 'r'],
+    ['m', 'a', 'g', 'i', 'c', 'k'],
+    ['o', 'c', 'c', 'u', 'l', 't'],
+    ['s', 'u', 'c', 'c', 'u', 'b', 'u', 's']
+    ['s', 'u', 'm', 'm', 'o', 'n'],
+    ['r', 'i', 't', 'u', 'a', 'l'],
+    ['c', 'r', 'o', 'w', 'l', 'e', 'y'],
+    ['s', 'a', 'c', 'r', 'i', 'f', 'i', 'c', 'e'],
+    ['a', 'l', 't', 'a', 'r']
 ]
             
 
-console.log(words[2].length);
-console.log(words[1].length);
-console.log(words[0].length);
-console.log(words[0][3]);
-console.log(words[2][2]);
+console.log(words[2]);
