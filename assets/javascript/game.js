234247952dd545c2eac1bd390cfc599dd6e0b318
Greatest Dane <DaneGoodman85@gmail.com>
2018-04-29 18:51:56 -0700

    reset.css linked

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+
+
+var words = [
+    ['m', 'a', 'd', 'd', 'o', 'n', 'n', 'a'],
+    ['r', 'a', 'g', 'e'],
+    ['d', 'e', 'a', 't', 'h']
+]
+            
+
+console.log(words[2].length);
+console.log(words[1].length);
+console.log(words[0].length);
+console.log(words[0][3]);
+console.log(words[2][2]);
