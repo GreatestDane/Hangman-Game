232ad70fb0934ba96ab36a342780af3d5141a4a7
Greatest Dane <DaneGoodman85@gmail.com>
2018-04-30 15:07:00 -0700

    I've got some of the basic logic set up for the game, still not  there just yet.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,24 @@ var losses = 0;
 var guessesLeft = 12;
 var userGuess;
 var randomWord;
+var currentWord = [];
+var letterSwap;
+
+// Declare the span variables //
+var spanWins = document.getElementById("wins");
+var spanLosses = document.getElementById("losses");
+var spanCurrentWord = document.getElementById("current-word");
+var spanGuessesRemaining = document.getElementById("guesses-remaining");
+var spanAlreadyGuessed = document.getElementById("already-guessed");
+
+// Declare functions for game //
+
+function displayWord(array) {
+    for (var i = 0; i < array.length; i ++) {
+        currentWord.push("-");
+        spanCurrentWord.textContent = currentWord;
+    }
+}
 
 // Create the array of array's for the words you're going to have //
 var words = [
@@ -21,6 +39,37 @@ var words = [
     ['s', 'a', 'c', 'r', 'i', 'f', 'i', 'c', 'e'],
     ['a', 'l', 't', 'a', 'r']
 ]
-            
 
-console.log(words[2]);
+// Create the array for the letters that are going to be guessed //
+var lettersGuessed = [];
+            
+// Generate a random word //
+randomWord = words[Math.floor(Math.random() * words.length)];
+console.log(randomWord);
+
+// present random word and number of guesses to the screen //
+displayWord(randomWord);
+spanGuessesRemaining.textContent = guessesLeft;
+
+// Begin game by pressing any key on the keyboard //
+
+document.onkeyup = function(event) {
+    userGuess = event.key;
+    // push userGuess into the array LettersGuessed and display it to screen //
+    lettersGuessed.push(userGuess);
+    spanAlreadyGuessed.textContent = lettersGuessed;
+    guessesLeft --;
+    spanGuessesRemaining.textContent = guessesLeft;
+
+    // begin logic for game //
+
+    if (randomWord.lastIndexOf(userGuess) > -1) {
+        alert(userGuess + " is present")
+        letterSwap = randomWord.indexOf(userGuess);
+        alert(letterSwap);
+        currentWord[letterSwap] = userGuess
+        spanCurrentWord.textContent = currentWord;
+    }
+
+
+}
